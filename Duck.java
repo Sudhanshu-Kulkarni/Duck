fdc3c7de5bfe43b55c309abd885b523153d88ce8
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-03-08 20:39:09 +0530

    Added setter and performer for QuackBehaviour

--- Duck.java
+++ Duck.java
@@ -3,6 +3,12 @@ abstract public class Duck {
   SwimBehaviour swimBehaviour;
   FlyBehaviour flyBehaviour;
 
+  public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+    this.quackBehaviour = quackBehaviour;
+  }
 
+  public void performQuack() {
+    quackBehaviour.quack();
+  }
 
 }
